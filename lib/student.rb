525e7081929f730076d80d24baf8e40279bc9e98
Randy Taylor <randyisbusy@gmail.com>
2019-09-26 08:55:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -87,8 +87,8 @@ end
       sql = <<-LOVE
       SELECT * FROM students WHERE grade = 10 LIMIT 1
               LOVE
-      y = DB[:conn].execute(sql).map do |row|
-        self.new_from_db(row)
+       DB[:conn].execute(sql).map do |row|
+      y =  self.new_from_db(row)
         binding.pry
   end
 end
