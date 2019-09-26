63b7871c64ded5a9422700a16b4cf3402a434373
Randy Taylor <randyisbusy@gmail.com>
2019-09-26 08:41:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -78,6 +78,7 @@ end
         SELECT * FROM students WHERE grade = 10 LIMIT ?
             SQLL
       DB[:conn].execute(sql).map do |row|
+        binding.pry
         self.new_from_db(row)
       end
  end
