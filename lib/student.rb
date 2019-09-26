edea88cf3df3feab3d5259bc312b40180de41d74
Randy Taylor <randyisbusy@gmail.com>
2019-09-26 08:39:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -73,7 +73,12 @@ end
     end
  end
 
- def self.first_X_students_in_grade_10
-
+ def self.first_X_students_in_grade_10(grade)
+    sql = <<-SQLL
+        SELECT * FROM students WHERE grade = ? 
+            SQLL
+      DB[:conn].execute(sql).map do |row| 
+        self.new_from_db(row) 
+      end
  end
 end
