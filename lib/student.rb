1b754fd8dd3a5419a49f5e7aa2fe93fe5e110f5b
Randy Taylor <randyisbusy@gmail.com>
2019-09-26 08:24:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -17,8 +17,8 @@ class Student
     sql = <<-YOO
       SELECT * FROM students
              YOO
-    DB[:conn].execute(sql).map do |row| 
-      self.new_from_db(row) 
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
     end
   end
 
